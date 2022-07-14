669e3a3e994820e2c9f037aa00adc4d0884f5ca2
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-07-14 23:32:28 +0500

    added StockController

--- src/main/java/com/example/tinkoffservice/controller/StockController.java
+++ src/main/java/com/example/tinkoffservice/controller/StockController.java
@@ -1,11 +1,13 @@
 package com.example.tinkoffservice.controller;
 
+import com.example.tinkoffservice.dto.FigiesDTO;
+import com.example.tinkoffservice.dto.StocksDTO;
+import com.example.tinkoffservice.dto.StocksPricesDTO;
+import com.example.tinkoffservice.dto.TickersDTO;
 import com.example.tinkoffservice.model.Stock;
 import com.example.tinkoffservice.service.StockService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,5 +17,15 @@ public class StockController {
     @GetMapping("/stocks/{ticker}")
     public Stock getStock(@PathVariable String ticker) {
         return stockService.getStockByTicker(ticker);
+    }
+
+    @PostMapping("/stocks/getStocksByTickers")
+    public StocksDTO getStocksByTickers(@RequestBody TickersDTO tickersDTO) {
+        return stockService.getStockByTickers(tickersDTO);
+    }
+
+    @PostMapping("/prices")
+    public StocksPricesDTO getPrices(@RequestBody FigiesDTO figiesDTO) {
+        return stockService.getPrices(figiesDTO);
     }
 }
