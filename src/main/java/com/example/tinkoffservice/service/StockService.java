3a270f42ad9fec49ecb44e2aa351300a97b858c4
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-07-14 23:24:30 +0500

    added methods

--- src/main/java/com/example/tinkoffservice/service/StockService.java
+++ src/main/java/com/example/tinkoffservice/service/StockService.java
@@ -1,7 +1,15 @@
 package com.example.tinkoffservice.service;
 
+import com.example.tinkoffservice.dto.FigiesDTO;
+import com.example.tinkoffservice.dto.StocksDTO;
+import com.example.tinkoffservice.dto.StocksPricesDTO;
+import com.example.tinkoffservice.dto.TickersDTO;
 import com.example.tinkoffservice.model.Stock;
 
 public interface StockService {
     Stock getStockByTicker(String ticker);
+
+    StocksDTO getStockByTickers(TickersDTO tickersDTO);
+
+    StocksPricesDTO getPrices(FigiesDTO figiesDTO);
 }
